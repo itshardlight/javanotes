195b101238fb1a6a9c6e4dc9fd19966996a18e5f
unish <unishmhrjan@gmail.com>
2026-06-16 23:11:42 +0545

    variables

---
+++ src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args){
+
+        // Premitive Variables
+        int rollNumber = 12;
+        int houseNumber = 101;
+        
+        double gpa = 1.2;
+        double carMileage = 34.5;
+
+        char grade = 'A';
+        char currency = '$';
+        
+        boolean forSale = true;
+        boolean isStudent = false;
+
+        // Reference Variables
+        String name = "Unish";
+        String email = "fake123@gmail.com";
+        // we can add a numbers but it is treated as a char rather than an int
+
+        
+    }
+}
