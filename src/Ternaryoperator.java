b59dbc33bca9bf16a56cbf2a5795e78827b5a4fe
unish <unishmhrjan@gmail.com>
2026-06-17 20:33:56 +0545

    ternary operator

---
+++ src/Ternaryoperator.java
@@ -0,0 +1,7 @@
+public class Ternaryoperator {
+    public static void main(String[] args) {
+        //it is the replacement of the if else condition 
+        // (condition) ? "if":"else"
+        System.out.println((1>2)?"True":"False");
+    }
+}
