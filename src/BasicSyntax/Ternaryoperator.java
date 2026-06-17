b40d9b18953789792912c8df213727f0302992b0
unish <unishmhrjan@gmail.com>
2026-06-17 20:51:15 +0545

    enhanced swithces

---
+++ src/BasicSyntax/Ternaryoperator.java
@@ -0,0 +1,8 @@
+package BasicSyntax;
+public class Ternaryoperator {
+    public static void main(String[] args) {
+        //it is the replacement of the if else condition 
+        // (condition) ? "if":"else"
+        System.out.println((1>2)?"True":"False");
+    }
+}
