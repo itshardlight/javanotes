02c22a5c33376d5b8a046014f477aad54af758b3
unish <unishmhrjan@gmail.com>
2026-06-17 20:51:17 +0545

    hi

--- src/Arithmeticoperator.java
+++ src/Arithmeticoperator.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 public class Arithmeticoperator {
     public static void main(String[] args){
---
+++ src/Enhancedswitches.java
@@ -0,0 +1,11 @@
+
+
+public class Enhancedswitches {
+    public static void main(String[] args) {
+        int integer = 1;
+        switch(integer){
+            case 1 -> System.out.println("nice");
+            case 2 -> System.out.println("wow");
+        }
+    }
+}
