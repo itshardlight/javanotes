3e093a53fef26831b393375dd61b4f7c48f813ee
unish <unishmhrjan@gmail.com>
2026-06-18 23:34:27 +0545

    for loop

---
+++ src/Forloop.java
@@ -0,0 +1,8 @@
+public class Forloop {
+ public static void main(String[] args) {
+    // initializtion;condition;incremt or decrement;
+    for(int i =0;i<3;i++){
+        System.out.println("this is the value of i in each iteration "+i);
+    }
+ }   
+}
