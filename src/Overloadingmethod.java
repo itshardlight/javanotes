7c037b0ce97473edc15113dd2939cad4f88217a5
unish <unishmhrjan@gmail.com>
2026-06-18 23:57:16 +0545

    overloading method

---
+++ src/Overloadingmethod.java
@@ -0,0 +1,13 @@
+public class Overloadingmethod {
+    //make sure to use a dif parameter but the method name can be same as given
+    static int add(int a, int b){
+        return(a+b);
+    }
+    static int add(int a, int b,int c){
+        return(a+b+c);
+    }
+    public static void main(String[] args) {
+        System.out.println(add(1,2));
+        System.out.println(add(3,4,5));
+    }
+}
